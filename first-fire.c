88ba5bc1ab937bbf67df6cbae688e607dba852cd
Solomon.Chidera <116091273+SolomonChidera@users.noreply.github.com>
2023-09-08 04:31:35 +0100

    Update first-fire.c/##Made changes on some loop

--- first-fire.c
+++ first-fire.c
@@ -14,13 +14,15 @@ int fireColorPalette[FIRE_COLORS][3];
 
 void initializeFire() {
 	for (int x = 0; x < FIRE_WIDTH; x++) {
-		for (int y = 0; y < FIRE_HEIGHT; y++) {
+		for (int y = 0; y < FIRE_HEIGHT; y++)
+		{
 			firePixels[x][y] = 0;
 		}
 	}
 
-	// Initialize the fire color palette
-	for (int i = 0; i < FIRE_COLORS; i++) {
+	/* Initialize the fire color palette */
+	for (int i = 0; i < FIRE_COLORS; i++)
+	{
 		fireColorPalette[i][0] = i * 7;
 		fireColorPalette[i][1] = 0;
 		fireColorPalette[i][2] = 0;
