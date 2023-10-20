795a387bedc07a8e03921e1a3cd4570b8ac666ae
SolomonChidera <solochidera10@gmail.com>
2023-10-20 23:10:16 +0100

    Decided to work on this today

--- first-fire.c
+++ first-fire.c
@@ -66,8 +66,10 @@ void renderFire(SDL_Renderer *renderer)
 	}
 }
 
-int main(int argc, char* args[]) {
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+int main(int argc, char* args[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 		return -1;
 	}
