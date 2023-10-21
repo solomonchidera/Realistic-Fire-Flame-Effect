06fa80a7bea3945fa75545ecb718319671088fc8
SolomonChidera <solochidera10@gmail.com>
2023-10-21 20:11:58 +0100

    Update/on fire.c file

--- src/fire.c
+++ src/fire.c
@@ -1,13 +1,4 @@
-#include <SDL2/SDL.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-#define FIRE_WIDTH 80
-#define FIRE_HEIGHT 60
-#define FIRE_COLORS 36
+#include "fire.h"
 
 int firePixels[FIRE_WIDTH][FIRE_HEIGHT];
 int fireColorPalette[FIRE_COLORS][3];
